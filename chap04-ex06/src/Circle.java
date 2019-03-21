bdf9c8af03cf608a41e0342ea306f44e396b9dce
USER <USER@AMP-10>
2019-03-21 15:38:03 +0900

    commit

---
+++ chap04-ex06/src/Circle.java
@@ -0,0 +1,16 @@
+
+public class Circle {
+	int radius;
+	
+	
+	public Circle(int radius) {
+		this.radius = radius;
+	
+		
+	}
+
+	
+	public double getArea() {
+		return 3.14 * radius* radius;
+	}
+}
