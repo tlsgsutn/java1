fb579f8c4274a2c6d9ca0af9d7fd56646e36876d
USER <USER@AMP-05>
2019-04-18 17:26:11 +0900

    commit

--- chap10-ex01/src/BorderLayoutEx.java
+++ chap10-ex01/src/BorderLayoutEx.java
@@ -1,9 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
 
-public class BorderLayoutEx {
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class BorderLayoutEx extends JFrame {
+	public BorderLayoutEx() {
+		setTitle("BorderLayout Sample");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		Container c = getContentPane();
+
+		c.setLayout(new BorderLayout(30, 20));
+		c.add(new JButton("Calculate"), BorderLayout.CENTER);
+		c.add(new JButton("add"), BorderLayout.NORTH);
+		c.add(new JButton("sub"), BorderLayout.SOUTH);
+		c.add(new JButton("mul"), BorderLayout.EAST);
+		c.add(new JButton("div"), BorderLayout.WEST);
+
+
+		setSize(300, 200); // 프레임 크기 300x200 설정
+		setVisible(true); // 프레임을 화면에 출력
 	}
 
+	public static void main(String[] args) {
+		new BorderLayoutEx();
+
+	}
 }
